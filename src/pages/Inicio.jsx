863996784fc8c7570f34b24a22f8dc9da57afa9c
Mujicash <andre27012001@gmail.com>
2022-08-18 15:38:15 -0500

    Ordenes de Compra filtradas por ruc del proveedor

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -7,99 +7,99 @@ import axios from "axios";
 
 const cookies = new Cookies();
 
-class Inicio extends React.Component{
+class Inicio extends React.Component {
 
-    cerrarSesion(e){
+    cerrarSesion(e) {
         e.preventDefault();
 
         const config = {
-            headers:{
-                "Authorization": `Bearer ${cookies.get('token')}` 
+            headers: {
+                "Authorization": `Bearer ${cookies.get('token')}`
             }
-          };
+        };
 
 
-          axios.get(`http://localhost:8000/api/logout?id_usuario=${parseInt(cookies.get('id'))}&tipo=${cookies.get('tipo')}`, config)
-          .then(res =>{
-            console.log(res);
-          })
-          .catch(err =>{
-              console.log(err);
-          })
+        axios.get(`http://localhost:8000/api/logout?id_usuario=${parseInt(cookies.get('id'))}&tipo=${cookies.get('tipo')}`, config)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.log(err);
+            })
 
-        cookies.remove('id',{path:'/'});
-        cookies.remove('nombre',{path:'/'});
-        cookies.remove('apellidoPaterno',{path:'/'});
-        cookies.remove('apellidoMaterno',{path:'/'});
-        cookies.remove('tipo',{path:'/'});
-        cookies.remove('sucursal',{path:'/'});
-        cookies.remove('usuario',{path:'/'});
-        cookies.remove('token',{path:'/'});
+        cookies.remove('id', { path: '/' });
+        cookies.remove('nombre', { path: '/' });
+        cookies.remove('apellidoPaterno', { path: '/' });
+        cookies.remove('apellidoMaterno', { path: '/' });
+        cookies.remove('tipo', { path: '/' });
+        cookies.remove('sucursal', { path: '/' });
+        cookies.remove('usuario', { path: '/' });
+        cookies.remove('token', { path: '/' });
 
-        window.location.href='/';
+        window.location.href = '/';
     }
 
-    componentDidMount(){
-        if(!cookies.get('id')){
+    componentDidMount() {
+        if (!cookies.get('id')) {
             window.location.href = '/';
         }
     }
 
-    abrirModal(e){
+    abrirModal(e) {
         e.preventDefault();
         document.getElementById('dia-sino').showModal();
     }
 
-    cerrarModal(e){
+    cerrarModal(e) {
         e.preventDefault();
         document.getElementById('dia-sino').close();
     }
 
-    abrirModalFinal(e){
+    abrirModalFinal(e) {
         e.preventDefault();
         document.getElementById('dia-sino').close();
         document.getElementById('dia-conf').showModal();
     }
 
-    
 
-    render(){
-        return(
+
+    render() {
+        return (
             <>
-            <div className="cont_card">
+                <div className="cont_card">
 
                     <CardUsuario
-                        cargo = {cookies.get('tipo')}
-                        nombre = {cookies.get('nombre')} 
-                        usuario = {cookies.get('usuario')}
-                        direccion = {cookies.get('direccion')}
-                        sucursal = {cookies.get('sucursal')}
-                        foto = 'shawn.png'
+                        cargo={cookies.get('tipo')}
+                        nombre={cookies.get('nombre')}
+                        usuario={cookies.get('usuario')}
+                        direccion={cookies.get('direccion')}
+                        sucursal={cookies.get('sucursal')}
+                        foto='shawn.png'
                     />
-                    <button 
+                    <button
                         className="btn_cerrar_sesion"
-                        onClick={this.abrirModal}    
+                        onClick={this.abrirModal}
                     >
                         Cerrar Sesión
                     </button>
 
-            </div>
-            <PopUpSiNo
-                idd='dia-sino'
-                titulo='cerrar sesion'
-                texto='¿Desea cerrar sesion?'
-                si={this.abrirModalFinal}
-                no={this.cerrarModal}
-                color= 'var(--primary)'
-            />
-            <PopUpConfirmacion
-                idd='dia-conf'
-                titulo='cerrar sesion'
-                texto='Adios'
-                button = 'Aceptar'
-                cerrar = {this.cerrarSesion}
-                color= 'var(--primary)'
-            />
+                </div>
+                <PopUpSiNo
+                    idd='dia-sino'
+                    titulo='cerrar sesion'
+                    texto='¿Desea cerrar sesion?'
+                    si={this.abrirModalFinal}
+                    no={this.cerrarModal}
+                    color='var(--primary)'
+                />
+                <PopUpConfirmacion
+                    idd='dia-conf'
+                    titulo='cerrar sesion'
+                    texto='Adios'
+                    button='Aceptar'
+                    cerrar={this.cerrarSesion}
+                    color='var(--primary)'
+                />
             </>
         );
     }
--- src/pages/OrdenesCompa.jsx
+++ src/pages/OrdenesCompa.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import BarraRegistroFiltro  from "../components/BarraRegistroFiltro";
+import BarraRegistroFiltro from "../components/BarraRegistroFiltro";
 import '../css/Usuarios.css';
 import ContButtonsTab from "../components/ContButtonsTab";
 import ButtonTab from "../components/ButtonTab";
@@ -11,38 +11,38 @@ import DetalleOrdenCompra from '../components/DetalleOrdenCompra'
 const cookies = new Cookies();
 
 const config = {
-    headers:{
-        "Authorization": `Bearer ${cookies.get('token')}` 
+    headers: {
+        "Authorization": `Bearer ${cookies.get('token')}`
     }
 };
 
 const infoUser = `?id_usuario=${parseInt(cookies.get("id"))}&tipo=${cookies.get("tipo")}`;
 
-const Fila =(props)=>{
-    return(
+const Fila = (props) => {
+    return (
         <tr>
-        <td>{props.idd}</td>
-        <td>{props.fecha}</td>
-        <td>{props.ruc}</td>
-        <td>{props.costo}</td>
-        <td>{props.estado}</td>
-        <td>
-            <ContButtonsTab>
-                <ButtonTab
-                    texto='Detallar'
-                    back='var(--normal)'
-                    border='var(--normal-dark)'
-                    click={props.detallar}
-                />
-            </ContButtonsTab>
-        </td>
+            <td>{props.idd}</td>
+            <td>{props.fecha}</td>
+            <td>{props.ruc}</td>
+            <td>{props.costo}</td>
+            <td>{props.estado}</td>
+            <td>
+                <ContButtonsTab>
+                    <ButtonTab
+                        texto='Detallar'
+                        back='var(--normal)'
+                        border='var(--normal-dark)'
+                        click={props.detallar}
+                    />
+                </ContButtonsTab>
+            </td>
 
         </tr>
     );
 }
 
-const FilaDetalle =(props)=>{
-    return(
+const FilaDetalle = (props) => {
+    return (
         <tr>
             <td>{props.codigo}</td>
             <td>{props.producto}</td>
@@ -55,102 +55,119 @@ const FilaDetalle =(props)=>{
 
 let ordenes = [];
 
-class OrdenCompra extends React.Component{
+class OrdenCompra extends React.Component {
 
     constructor() {
         super();
-    
+
         this.state = {
-            ordenes : {
-                data : []
+            ordenes: {
+                data: []
             },
-            busqueda : '',
-            detalleOrden : {
-                numero : '',
-                fecha : '',
-                estado : '',
+            busqueda: '',
+            detalleOrden: {
+                numero: '',
+                fecha: '',
+                estado: '',
                 empresa: '',
                 ruc: '',
-                productos : [],
-                total : ''
+                productos: [],
+                total: ''
             }
         };
     }
 
-    traerOrdenes = async ()=>{ 
-            await axios.get(`http://localhost:8000/api/ordenesCompra${infoUser}`, config)
-            .then(res =>{
+    traerOrdenes = async () => {
+        await axios.get(`http://localhost:8000/api/ordenesCompra${infoUser}`, config)
+            .then(res => {
                 // console.log(res.data);
                 ordenes = res.data;
                 this.setState({
-                    ordenes:{
+                    ordenes: {
                         data: ordenes
                     }
                 });
                 console.log(ordenes);
             })
-            .catch(err =>{
+            .catch(err => {
                 console.log(err);
             })
     }
 
-    traerDataOrden = async (id)=>{
+    traerDataOrden = async (id) => {
         await axios.get(`http://localhost:8000/api/ordenesCompra/${id}${infoUser}`, config)
-            .then(res =>{
+            .then(res => {
                 let totOrden = 0;
                 console.log(res.data);
                 res.data.productos.map(p => {
                     totOrden = totOrden + (parseInt(p.cantidad) * parseFloat(p.precio))
                 })
                 this.setState({
-                    detalleOrden : {
-                        numero : res.data.id,
-                        fecha : res.data.fecha_compra,
-                        estado : res.data.estado,
+                    detalleOrden: {
+                        numero: res.data.id,
+                        fecha: res.data.fecha_compra,
+                        estado: res.data.estado,
                         empresa: res.data.nombre_proveedor,
                         ruc: res.data.ruc_proveedor,
-                        productos : res.data.productos,
-                        total : totOrden
+                        productos: res.data.productos,
+                        total: totOrden
                     }
                 });
 
             })
-            .catch(err =>{
+            .catch(err => {
                 console.log(err);
             })
         document.getElementById('pop-detalle-oc').showModal();
     }
 
-    cerrarModal = ()=>{
+    capturarBusqueda = async (e) => {
+        e.persist();
+        await this.setState({ busqueda: e.target.value });
+        // console.log(this.state.busqueda);
+        this.filtraBusqueda();
+    }
+
+    filtraBusqueda = () => {
+        let search = ordenes.filter(i => {
+            if (i.rucProveedor.toLowerCase().includes(this.state.busqueda.toLowerCase())) {
+                return i;
+            }
+        });
+        console.log(search);
+        this.setState({ ordenes: { data: search } })
+    }
+
+    cerrarModal = () => {
         document.getElementById('pop-conf-oc').close();
         document.getElementById('pop-registrar-oc').close();
         this.traerOrdenes();
     }
 
-    componentDidMount(){
-        if(!cookies.get('id')){
+    componentDidMount() {
+        if (!cookies.get('id')) {
             window.location.href = '/';
         }
         this.traerOrdenes();
     }
 
-    abrirRegistro = () =>{
+    abrirRegistro = () => {
         document.getElementById('pop-registrar-oc').showModal();
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <>
-            <h1 className="tit_general">Orden de Compra</h1>
-            <BarraRegistroFiltro 
+                <h1 className="tit_general">Orden de Compra</h1>
+                <BarraRegistroFiltro
                     registro={true}
                     txtRegistro='Registrar Orden'
                     place='Proveedor...'
                     registrar={this.abrirRegistro}
-                    change = {this.capturarBusqueda}
+                    change={this.capturarBusqueda}
                 />
-            <div className="cont_table_user">
-                <table className="table_user">
+                <div className="cont_table_user">
+                    <table className="table_user">
                         <caption className="tit_table_user">
                             Lista de Ordenes de Compra
                         </caption>
@@ -167,14 +184,14 @@ class OrdenCompra extends React.Component{
                         </thead>
                         <tbody className="tbody_user">
                             {this.state.ordenes.data.map(u =>
-                                <Fila 
+                                <Fila
                                     key={u.idOrden}
                                     idd={u.idOrden}
-                                    fecha = {u.fechaCompra}
-                                    ruc = {u.rucProveedor}
-                                    costo = {`S/. ${u.costoTotal}`}
-                                    estado = {u.estado}
-                                    detallar = {() => this.traerDataOrden(u.idOrden)}
+                                    fecha={u.fechaCompra}
+                                    ruc={u.rucProveedor}
+                                    costo={`S/. ${u.costoTotal}`}
+                                    estado={u.estado}
+                                    detallar={() => this.traerDataOrden(u.idOrden)}
                                 />
                             )}
                         </tbody>
@@ -182,31 +199,31 @@ class OrdenCompra extends React.Component{
                 </div>
                 <RegistrarOrdenCompra
                     idd='pop-registrar-oc'
-                    cerrar = {() => document.getElementById('pop-registrar-oc').close()}
+                    cerrar={() => document.getElementById('pop-registrar-oc').close()}
                     /*------------------------- */
-                    cerrarConf = {this.cerrarModal}
+                    cerrarConf={this.cerrarModal}
                 />
                 <DetalleOrdenCompra
                     idd='pop-detalle-oc'
-                    numero  = {this.state.detalleOrden.numero}
-                    fecha  = {this.state.detalleOrden.fecha}
-                    estado  ={this.state.detalleOrden.estado}
-                    empresa = {this.state.detalleOrden.empresa}
-                    ruc = {this.state.detalleOrden.ruc}
-                    total = {this.state.detalleOrden.total}
+                    numero={this.state.detalleOrden.numero}
+                    fecha={this.state.detalleOrden.fecha}
+                    estado={this.state.detalleOrden.estado}
+                    empresa={this.state.detalleOrden.empresa}
+                    ruc={this.state.detalleOrden.ruc}
+                    total={this.state.detalleOrden.total}
                     /*------------------------------------*/
-                    volver = {()=>document.getElementById('pop-detalle-oc').close()}
-                    generar= {()=> console.log('...Generar Reporte...') }
+                    volver={() => document.getElementById('pop-detalle-oc').close()}
+                    generar={() => console.log('...Generar Reporte...')}
                 >
-                    {this.state.detalleOrden.productos.map(d => 
+                    {this.state.detalleOrden.productos.map(d =>
                         <FilaDetalle
                             key={d.id}
                             codigo={d.id}
                             cantidad={d.cantidad}
                             producto={d.nombre}
-                            costo= {d.precio}
+                            costo={d.precio}
                             subtotal={parseInt(d.cantidad) * parseFloat(d.precio)}
-                    />)}
+                        />)}
 
                 </DetalleOrdenCompra>
             </>
