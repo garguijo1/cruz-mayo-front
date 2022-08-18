82360e4a8c9f3d5502dfb6d039c6e79476900ba8
Mujicash <andre27012001@gmail.com>
2022-08-18 12:47:36 -0500

    Slidebar filtrado

--- src/components/Slidebar.jsx
+++ src/components/Slidebar.jsx
@@ -1,15 +1,38 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
+import Cookies from "universal-cookie";
 
-const Slidebar = () => {
-    return (
-        <nav className="navbar">
+
+const cookies = new Cookies();
+const tipo = cookies.get("tipo")
+
+
+const menuTipo = () => {
+
+    if (tipo === "Administrador") {
+        return (
             <ul className="list_link_navbar">
                 <li className="li_navbar">
                     <NavLink to='/con/inicio'>Inicio</NavLink>
                 </li>
                 <li className="li_navbar">
+                    <NavLink to='/con/usuarios'>Usuarios</NavLink>
+                </li>
+                <li className="li_navbar">
+                    <NavLink to='/con/proveedores'>Proveedores</NavLink>
+                </li>
+                <li className="li_navbar">
                     <NavLink to='/con/productos'>Productos</NavLink>
+                </li>
+            </ul>
+        );
+    }
+
+    if (tipo === "Jefe Almacen") {
+        return (
+            <ul className="list_link_navbar">
+                <li className="li_navbar">
+                    <NavLink to='/con/inicio'>Inicio</NavLink>
                 </li>
                 <li className="li_navbar">
                     <NavLink to='/con/proveedores'>Proveedores</NavLink>
@@ -23,14 +46,31 @@ const Slidebar = () => {
                 <li className="li_navbar">
                     <NavLink to='/con/guia_remision'>Guia de Remisión</NavLink>
                 </li>
+            </ul>
+        );
+    }
+
+    if (tipo === "Encargado Botica") {
+        return (
+            <ul className="list_link_navbar">
                 <li className="li_navbar">
-                    <NavLink to='/con/usuarios'>Usuarios</NavLink>
+                    <NavLink to='/con/inicio'>Inicio</NavLink>
+                </li>
+                <li className="li_navbar">
+                    <NavLink to='/con/stock'>Stock</NavLink>
                 </li>
                 <li className="li_navbar">
                     <NavLink to='/con/orden_salida'>Ordenes de Salida</NavLink>
                 </li>
-
             </ul>
+        );
+    }
+}
+
+const Slidebar = () => {
+    return (
+        <nav className="navbar">
+            {menuTipo()}
         </nav>
     );
 }
